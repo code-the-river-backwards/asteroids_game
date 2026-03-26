ac47bf1e6d215cc068573293e2158614435b4fe7
code-the-river-backwards <endzio32@gmail.com>
2026-03-26 12:59:22 +0100

    added splitting functionality to asteroids, after they are shot

--- asteroids_game/asteroid.py
+++ asteroids_game/asteroid.py
@@ -1,5 +1,7 @@
 from circleshape import *
 from constants import *
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -10,3 +12,27 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    # splitting the asteroids after it's shot - logic
+    def split(self):
+        self.kill()
+
+        # if it's already a small asteroid, do nothing
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return 
+        
+        # if not, splitting logic time!
+        log_event("asteroid_split")
+
+        random_angle = random.uniform(20, 50)
+
+        new_vector1 = self.velocity.rotate(random_angle)
+        new_vector2 = self.velocity.rotate(-random_angle)
+
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new_asteroid1 = Asteroid(x=self.position.x, y=self.position.y, radius=new_radius)
+        new_asteroid2 = Asteroid(x=self.position.x, y=self.position.y, radius=new_radius)
+
+        new_asteroid1.velocity = new_vector1 * 1.2
+        new_asteroid2.velocity = new_vector2 * 1.2
--- asteroids_game/main.py
+++ asteroids_game/main.py
@@ -70,6 +70,13 @@ def main():
                 print ("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid) == True:
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
 
         for thing in drawable:
             thing.draw(screen)
